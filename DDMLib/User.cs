402a42334dcdf1a5a48230274593364a985e966a
sellasouls666 <pgbc303@gmail.com>
2025-10-01 17:52:11 +0300

    сделал класс User

---
+++ DDMLib/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib
+{
+    public class User
+    {
+        public string Email { get; set; }           
+        public string Password { get; set; }       
+        public string FullName { get; set; }        
+        public string Phone { get; set; }   //сделать необязательным
+        public string Address { get; set; }     //сделать необязательным        
+        public DateTime RegistrationDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
