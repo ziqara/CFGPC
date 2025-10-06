614de7e71f44831acaf2fcf6483a5502fb60c883
sellasouls666 <pgbc303@gmail.com>
2025-10-06 17:07:10 +0300

    создал интерфейс IUserRepository

---
+++ DDMLib/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib
+{
+    public interface IUserRepository
+    {
+        User FindByEmail(string email);
+        User Save(User user);
+    }
+}
