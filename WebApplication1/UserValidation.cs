57c41a84613ce61aa5a0a7721d8a08ab165ea4a3
ziqara <165387888+ziqara@users.noreply.github.com>
2025-10-26 14:25:51 +0300

    Добавлена валидация на каждое поле из класса User

--- WebApplication1/UserValidation.cs
+++ WebApplication1/UserValidation.cs
@@ -4,5 +4,21 @@ namespace WebApplication1
 {
     public partial class UserValidation
     {
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress(ErrorMessage = "Неверный формат email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль обязателен")]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль должен быть от 6 до 20 символов")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Полное имя обязательно")]
+        public string FullName { get; set; }
+
+        [Phone(ErrorMessage = "Неверный формат телефона (только цифры и символы как +, -, ())")]
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
     }
 }
