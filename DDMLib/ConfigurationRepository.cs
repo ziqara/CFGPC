b6c1122a9cfbe7b38dc809f52ef359b965b86338
sellasouls666 <pgbc303@gmail.com>
2025-10-27 19:30:02 +0300

    создал ConfigurationRepository, IConfigurationRepository, метод GetUserConfigurations с заглушкой вместо реализации

---
+++ DDMLib/ConfigurationRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib
+{
+    public class ConfigurationRepository : IConfigurationRepository
+    {
+        public List<ConfigurationCard> GetUserConfigurations(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
