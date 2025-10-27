c6eaa25139af0d90d920df33473dbd20b6567be2
sellasouls666 <pgbc303@gmail.com>
2025-10-27 20:18:30 +0300

    создал OrderRepository и IOrderRepository, метод GetUserOrders с заглушкой вместо реализации

---
+++ DDMLib/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public List<OrderCard> GetUserOrders(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
