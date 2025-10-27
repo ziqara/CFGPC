e2b4c0ec0f74630c47f05391061529bd5fb68790
sellasouls666 <pgbc303@gmail.com>
2025-10-27 20:12:14 +0300

    создал класс OrderCard

---
+++ DDMLib/OrderCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib
+{
+    public class OrderCard
+    {
+        public int OrderId { get; set; }
+        public int ConfigId { get; set; }
+        public string UserEmail { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string DeliveryMethod { get; set; }
+        public string PaymentMethod { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
