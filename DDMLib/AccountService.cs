999b50619cd97c31dd7bdde310153b14410ee9b8
sellasouls666 <pgbc303@gmail.com>
2025-10-27 18:37:45 +0300

    создал класс AccountService и метод GetUserProfile в нём с заглушкой вместо реализации

---
+++ DDMLib/AccountService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib
+{
+    public class AccountService
+    {
+        public User GetUserProfile(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
