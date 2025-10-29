2c83e0dffe0f88d57cf998c2f3fba126b5446094
sellasouls666 <pgbc303@gmail.com>
2025-10-29 19:35:40 +0300

    создал класс ISessionManager

---
+++ DDMLib/ISessionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib
+{
+    public interface ISessionManager
+    {
+        bool ValidateSession();
+        string GetUserEmailFromSession();
+        void CreateSession(string email, string userName);
+        void InvalidateSession();
+        bool IsUserAuthenticated();
+    }
+}
