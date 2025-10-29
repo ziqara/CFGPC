60a0cee7a168ef35b557a2a516a218c477d2f335
ziqara <165387888+ziqara@users.noreply.github.com>
2025-10-29 20:58:57 +0300

    Добавлена инициализация моков и сервисов для тестов

---
+++ DDMTests/SuppliersDisplayTests.cs
@@ -0,0 +1,22 @@
+﻿// SuppliersDisplayTests.cs
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+[TestClass]
+public class SuppliersDisplayTests
+{
+    private Mock<ISupplierRepository> _repo;
+    private SupplierService _service;
+    private Mock<ISuppliersView> _view;
+    private SuppliersPresenter _presenter;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _repo = new Mock<ISupplierRepository>();
+        _service = new SupplierService(_repo.Object);
+        _view = new Mock<ISuppliersView>();
+        _presenter = new SuppliersPresenter(_service, _view.Object);
+    }
+}
