c2e77277f3fb1fe79300fb946d84cc7b99e46418
ziqara <165387888+ziqara@users.noreply.github.com>
2025-10-30 22:43:07 +0300

    Добавлен класс-сервис, а также конструктор класса, который принимает объект ISupplierRepository

---
+++ DDMLib/SupplierService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib
+{
+    public class SupplierService
+    {
+        private readonly ISupplierRepository _repo;
+
+        public SupplierService(ISupplierRepository repo)
+        {
+            if (repo == null)
+                throw new System.ArgumentNullException(nameof(repo));
+
+            _repo = repo;
+        }
+    }
+}
