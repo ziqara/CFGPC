707a0bfcce48b900653a36bbb59c92d38cd03822
ziqara <165387888+ziqara@users.noreply.github.com>
2025-10-30 22:54:22 +0300

    Кейс 2. Пустые поля (null / "")

--- DDMTests/TSuppliersService.cs
+++ DDMTests/TSuppliersService.cs
@@ -28,5 +28,46 @@ namespace DDMTests
             Assert.AreEqual(0, result.Count);
             repo.Verify(r => r.ReadAllSuppliers(), Times.Once);
         }
+
+        [TestMethod]
+        public void GetAllSuppliers_OptionalFieldsNullOrEmpty_PassedAsIs()
+        {
+            // Arrange
+            var repo = new Mock<ISupplierRepository>();
+            var service = new SupplierService(repo.Object);
+
+            List<Supplier> data = new List<Supplier>
+            {
+                new Supplier { Inn=300000001, Name="ООО ПустоNull", ContactEmail="null@example.com",
+                            Phone=null, Address=null },
+                new Supplier { Inn=300000002, Name="ООО ПустоStr",  ContactEmail="empty@example.com",
+                           Phone="", Address="" }
+            };
+
+            repo.Setup(r => r.ReadAllSuppliers()).Returns(data);
+
+            // Act
+            var result = service.GetAllSuppliers();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+
+            // Запись №1
+            Assert.AreEqual(300000001, result[0].Inn);
+            Assert.AreEqual("ООО ПустоNull", result[0].Name);
+            Assert.AreEqual("null@example.com", result[0].ContactEmail);
+            Assert.IsNull(result[0].Phone);
+            Assert.IsNull(result[0].Address);
+
+            // Запись №2
+            Assert.AreEqual(300000002, result[1].Inn);
+            Assert.AreEqual("ООО ПустоStr", result[1].Name);
+            Assert.AreEqual("empty@example.com", result[1].ContactEmail);
+            Assert.AreEqual("", result[1].Phone);
+            Assert.AreEqual("", result[1].Address);
+
+            repo.Verify(r => r.ReadAllSuppliers(), Times.Once);
+        }
     }
 }
