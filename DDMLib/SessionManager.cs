b850c54c9febc478ec43b4bd8f9d360598e45830
sellasouls666 <pgbc303@gmail.com>
2025-11-03 22:43:11 +0300

    реализовал конструктор класса SessionManager

--- DDMLib/SessionManager.cs
+++ DDMLib/SessionManager.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace DDMLib
 {
     public class SessionManager : ISessionManager
     {
+        private readonly IHttpContextAccessor httpContextAccessor_;
 
+        public SessionManager(IHttpContextAccessor httpContextAccessor)
+        {
+            httpContextAccessor_ = httpContextAccessor;
+        }
     }
 }
