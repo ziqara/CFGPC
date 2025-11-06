dee1d20df183eb50e54da5a60d368f06a74d8ee8
ziqara <165387888+ziqara@users.noreply.github.com>
2025-11-06 21:54:02 +0300

    Каждая строка таблицы превращается в объект Supplier

--- DDMLib/Supplier/MySqlSupplierRepository.cs
+++ DDMLib/Supplier/MySqlSupplierRepository.cs
@@ -19,6 +19,19 @@ namespace DDMLib
 
                     string sql = "SELECT inn, name, contact_email, phone, address FROM suppliers";
                     MySqlCommand command = new MySqlCommand(sql, connection);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Supplier s = new Supplier(reader.GetInt32(0));
+                            s.Name = reader.GetString(1);
+                            s.ContactEmail = reader.GetString(2);
+                            s.Phone = reader.IsDBNull(3) ? null : reader.GetString(3);
+                            s.Address = reader.IsDBNull(4) ? null : reader.GetString(4);
+                            suppliers.Add(s);
+                        }
+                    }
                 }
             }
             catch 
