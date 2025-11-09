5cce19d60d462b9def73d8c7e25b6019c48c8293
sellasouls666 <pgbc303@gmail.com>
2025-11-09 21:24:27 +0300

    создал модель страницы UserProfile для отображения данных о пользователе

--- WebApplication1/Pages/UserProfile.cshtml.cs
+++ WebApplication1/Pages/UserProfile.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DDMLib;
 
-namespace ClientWebApp.Pages
+namespace WebApplication1.Pages
 {
     public class UserProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly SessionManager _sessionManager;
+        private readonly AccountService _accountService;
+
+        public UserProfileModel(SessionManager sessionManager, AccountService accountService)
         {
+            _sessionManager = sessionManager;
+            _accountService = accountService;
+        }
+
+        public User UserProfile { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if (!_sessionManager.IsUserAuthenticated())
+            {
+                return RedirectToPage("/Login");
+            }
+
+            var userEmail = _sessionManager.GetUserEmailFromSession();
+            if (string.IsNullOrEmpty(userEmail))
+            {
+                return RedirectToPage("/Login");
+            }
+
+            UserProfile = _accountService.GetUserProfile(userEmail);
+
+            return Page();
         }
     }
 }
