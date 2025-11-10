3c256d5b479775f9b6bf15120a4c77ab9f201c26
ziqara <165387888+ziqara@users.noreply.github.com>
2025-11-10 21:44:18 +0300

    Созданы шаблоны - главная форма, поставщики

---
+++ AdminForm/SupplierForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AdminForm
+{
+    partial class SupplierForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.supplierGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.supplierGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // supplierGridView
+            // 
+            this.supplierGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.supplierGridView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.supplierGridView.Location = new System.Drawing.Point(0, 66);
+            this.supplierGridView.Name = "supplierGridView";
+            this.supplierGridView.Size = new System.Drawing.Size(800, 384);
+            this.supplierGridView.TabIndex = 0;
+            // 
+            // SupplierForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.supplierGridView);
+            this.Name = "SupplierForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Supplier";
+            ((System.ComponentModel.ISupportInitialize)(this.supplierGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView supplierGridView;
+    }
+}
--- AdminForm/SupplierForm.cs
+++ AdminForm/SupplierForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AdminForm
 {
-    public partial class Form1 : Form
+    public partial class SupplierForm : Form
     {
-        public Form1()
+        public SupplierForm()
         {
             InitializeComponent();
         }
