fe08e19b87699fcf8adc5e82654bdcbe62792294
sellasouls666 <pgbc303@gmail.com>
2025-11-12 22:25:20 +0300

    создал класс CaseSpec

---
+++ DDMLib/Component/CaseSpec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib.Component
+{
+    public class CaseSpec
+    {
+        public string FormFactor { get; set; }
+        public string Size { get; set; }
+    }
+}
