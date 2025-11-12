99c03785faf41348a16f28716391f4a9116408f8
sellasouls666 <pgbc303@gmail.com>
2025-11-12 22:29:19 +0300

    заменил internal на public

--- DDMLib/Component/IComponentRepository.cs
+++ DDMLib/Component/IComponentRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DDMLib.Component
 {
-    internal interface IComponentRepository
+    public interface IComponentRepository
     {
         List<ComponentDto> GetComponentsByCategory(string category);
     }
