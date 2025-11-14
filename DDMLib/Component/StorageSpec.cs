35d0de17e48c948f7ba83d4ae338bbfe89679d0e
sellasouls666 <pgbc303@gmail.com>
2025-11-14 09:50:33 +0300

    разработал модель StorageSpec

---
+++ DDMLib/Component/StorageSpec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib.Component
+{
+    public class StorageSpec
+    {
+        public string Interface { get; set; }
+        public int CapacityGb { get; set; }
+    }
+}
