4ab2ba3a1db5b6760324e170a557c4cfe18bf9ce
ziqara <tcvegor2006@mail.ru>
2025-11-14 10:30:02 +0300

    Заглушки для тестов

---
+++ DDMLib/Supplier/SupplierValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib.Supplier
+{
+    public class SupplierValidator
+    {
+        public List<string> Validate(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
