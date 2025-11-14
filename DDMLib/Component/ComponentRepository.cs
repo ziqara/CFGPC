397b6d3cda1e5ab694246d6f1ce54b98d638a4fc
sellasouls666 <pgbc303@gmail.com>
2025-11-14 10:06:18 +0300

    добавил проверку аргумента category в методе GetComponentsByCategory

--- DDMLib/Component/ComponentRepository.cs
+++ DDMLib/Component/ComponentRepository.cs
@@ -8,6 +8,23 @@ namespace DDMLib.Component
 {
     public class ComponentRepository : IComponentRepository
     {
+        public List<ComponentDto> GetComponentsByCategory(string category)
+        {
+            if (string.IsNullOrWhiteSpace(category))
+            {
+                throw new ArgumentException("Категория не может быть пустой.", nameof(category));
+            }
 
+            HashSet<string> validCategories = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                "cpu", "motherboard", "ram", "gpu", "storage", "psu", "case", "cooling"
+            };
+
+            if (!validCategories.Contains(category))
+            {
+                throw new ArgumentException("Недопустимое значение категории.", nameof(category));
+            }
+
+        }
     }
 }
