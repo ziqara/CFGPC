38d9b61030fc43ffc69cb0bd41b5e1fd22548eaf
sellasouls666 <pgbc303@gmail.com>
2025-11-15 08:41:04 +0300

    Добавил пустую страницу Components

---
+++ WebApplication1/Pages/Components.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClientWebApp.Pages
+{
+    public class ComponentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
