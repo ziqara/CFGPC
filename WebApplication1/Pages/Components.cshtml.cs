445538e9cdd8cc729d39714f8455a8667204acdd
sellasouls666 <pgbc303@gmail.com>
2025-11-18 08:18:40 +0300

    разработал модель страницы Components

--- WebApplication1/Pages/Components.cshtml.cs
+++ WebApplication1/Pages/Components.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using DDMLib.Component;
 
 namespace ClientWebApp.Pages
 {
-    public class ComponentsModel : PageModel
+    public class ComponentsPageModel : PageModel
     {
-        public void OnGet()
+        private readonly ComponentService componentService_;
+
+        public ComponentsPageModel(ComponentService componentService)
         {
+            componentService_ = componentService;
+        }
+
+        public List<ComponentDto> Components { get; set; } = new List<ComponentDto>();
+
+        public void OnGet([FromQuery] string category = "cpu")
+        {
+            Components = componentService_.GetComponentsByCategory(category);
         }
     }
 }
