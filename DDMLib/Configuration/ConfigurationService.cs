8028ddaa254eab2dcba68d083ec1d7b560ff418c
sellasouls666 <pgbc303@gmail.com>
2025-11-21 10:03:16 +0300

    создал класс ConfigurationService

---
+++ DDMLib/Configuration/ConfigurationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib.Configuration
+{
+    public class ConfigurationService
+    {
+        private readonly IConfigurationRepository configurationRepository_;
+
+        public ConfigurationService(IConfigurationRepository configurationRepository)
+        {
+            configurationRepository_ = configurationRepository ?? throw new ArgumentNullException(nameof(configurationRepository));
+        }
+    }
+}
