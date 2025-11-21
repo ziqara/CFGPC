d6dd4218d37e984eeabaf505d60155bd55d22f31
sellasouls666 <pgbc303@gmail.com>
2025-11-21 09:34:35 +0300

    создал класс ConfigurationDto

---
+++ DDMLib/Configuration/ConfigurationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DDMLib.Component;
+
+namespace DDMLib.Configuration
+{
+    public class ConfigurationDto
+    {
+        public Configuration Configuration { get; set; }
+        public List<DDMLib.Component.Component> Components { get; set; } = new List<DDMLib.Component.Component>();
+    }
+}
