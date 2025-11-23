a86080e4befa64800b1f0680870102c8e59b7419
ziqara <165387888+ziqara@users.noreply.github.com>
2025-11-23 23:15:15 +0300

    оформление кнопок и текстбоксов

---
+++ WindowsFormsApp1/EditSupplierForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DDMLib;
+
+namespace WindowsFormsApp1
+{
+    public partial class EditSupplierForm : Form
+    {
+        private readonly SupplierService service_;
+        private readonly Supplier supplier_;
+
+        public EditSupplierForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
