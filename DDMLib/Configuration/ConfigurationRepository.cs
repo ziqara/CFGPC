7bde6e34fe7e339c3e11737bd2dd3f59737ef826
sellasouls666 <pgbc303@gmail.com>
2025-11-25 08:17:30 +0300

    разработал метод MapConfigurationFromReader

--- DDMLib/Configuration/ConfigurationRepository.cs
+++ DDMLib/Configuration/ConfigurationRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,43 @@ namespace DDMLib.Configuration
         public List<ConfigurationDto> GetUserConfigurations(string userEmail)
         {
             throw new NotImplementedException();
+        }
+
+        private Configuration MapConfigurationFromReader(MySqlDataReader reader)
+        {
+            // Получаем индексы колонок
+            int iId = reader.GetOrdinal("configId");
+            int iName = reader.GetOrdinal("configName");
+            int iDesc = reader.GetOrdinal("description");
+            int iPrice = reader.GetOrdinal("totalPrice");
+            int iTarget = reader.GetOrdinal("targetUse");
+            int iStatus = reader.GetOrdinal("status");
+            int iPreset = reader.GetOrdinal("isPreset");
+            int iDate = reader.GetOrdinal("createdDate");
+            int iEmail = reader.GetOrdinal("userEmail");
+            int iRgb = reader.GetOrdinal("rgb");
+            int iOther = reader.GetOrdinal("otherOptions");
+
+            // Функции для безопасного получения значений
+            Func<int, string> GetStringOrEmpty = i => reader.IsDBNull(i) ? string.Empty : reader.GetString(i);
+            Func<int, decimal> GetDecimalOrZero = i => reader.IsDBNull(i) ? 0m : reader.GetDecimal(i);
+            Func<int, bool> GetBoolOrFalse = i => reader.IsDBNull(i) ? false : reader.GetBoolean(i);
+            Func<int, DateTime> GetDateTimeOrMin = i => reader.IsDBNull(i) ? DateTime.MinValue : reader.GetDateTime(i);
+
+            return new Configuration
+            {
+                ConfigId = reader.GetInt32(iId),
+                ConfigName = GetStringOrEmpty(iName),
+                Description = GetStringOrEmpty(iDesc),
+                TotalPrice = GetDecimalOrZero(iPrice),
+                TargetUse = GetStringOrEmpty(iTarget),
+                Status = GetStringOrEmpty(iStatus),
+                IsPreset = GetBoolOrFalse(iPreset),
+                CreatedDate = GetDateTimeOrMin(iDate),
+                UserEmail = GetStringOrEmpty(iEmail),
+                Rgb = GetBoolOrFalse(iRgb),
+                OtherOptions = GetStringOrEmpty(iOther)
+            };
         }
     }
 }
