6bb3ac82e74f425ed400abb9f0366bcacc7c6c82
sellasouls666 <pgbc303@gmail.com>
2025-11-25 08:15:29 +0300

    реализоал интерфейс ConfigurationRepository

---
+++ DDMLib/Configuration/ConfigurationRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDMLib.Configuration
+{
+    public class ConfigurationRepository : IConfigurationRepository
+    {
+        public List<ConfigurationDto> GetUserConfigurations(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
