7e94aec729aeee8cfc07887f43b0016073321c43
AHOHUM <tcvegor2006@mail.ru>
2026-06-11 04:15:44 +0300

    авторизация обновлена

--- WindowsFormsApp1/LoginForm.Designer.cs
+++ WindowsFormsApp1/LoginForm.Designer.cs
@@ -33,55 +33,87 @@
             this.loginTextBox = new System.Windows.Forms.TextBox();
             this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.loginButton = new System.Windows.Forms.Button();
+            this.logobox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logobox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // logobox
+            // 
+            this.logobox.Location = new System.Drawing.Point(140, 25);
+            this.logobox.Name = "logobox";
+            this.logobox.Size = new System.Drawing.Size(120, 100);
+            this.logobox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logobox.TabIndex = 5;
+            this.logobox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.label1.Location = new System.Drawing.Point(40, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 35);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Войдите в систему";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // loginLabel
             // 
             this.loginLabel.AutoSize = true;
-            this.loginLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.loginLabel.Location = new System.Drawing.Point(45, 37);
+            this.loginLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.loginLabel.Location = new System.Drawing.Point(47, 185);
             this.loginLabel.Name = "loginLabel";
-            this.loginLabel.Size = new System.Drawing.Size(49, 16);
+            this.loginLabel.Size = new System.Drawing.Size(41, 15);
             this.loginLabel.TabIndex = 0;
-            this.loginLabel.Text = "Логин:";
-            // 
-            // passwordLabel
-            // 
-            this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.passwordLabel.Location = new System.Drawing.Point(34, 65);
-            this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(60, 16);
-            this.passwordLabel.TabIndex = 1;
-            this.passwordLabel.Text = "Пароль:";
+            this.loginLabel.Text = "Логин";
             // 
             // loginTextBox
             // 
             this.loginTextBox.BackColor = System.Drawing.Color.White;
-            this.loginTextBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.loginTextBox.Location = new System.Drawing.Point(100, 34);
+            this.loginTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loginTextBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.loginTextBox.Location = new System.Drawing.Point(50, 205);
             this.loginTextBox.Name = "loginTextBox";
-            this.loginTextBox.Size = new System.Drawing.Size(200, 22);
+            this.loginTextBox.Size = new System.Drawing.Size(300, 27);
             this.loginTextBox.TabIndex = 2;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.passwordLabel.Location = new System.Drawing.Point(47, 245);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(49, 15);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Пароль";
             // 
             // passwordTextBox
             // 
             this.passwordTextBox.BackColor = System.Drawing.Color.White;
-            this.passwordTextBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.passwordTextBox.Location = new System.Drawing.Point(100, 62);
+            this.passwordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordTextBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.passwordTextBox.Location = new System.Drawing.Point(50, 265);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.PasswordChar = '*';
-            this.passwordTextBox.Size = new System.Drawing.Size(200, 22);
+            this.passwordTextBox.PasswordChar = '•';
+            this.passwordTextBox.Size = new System.Drawing.Size(300, 27);
             this.passwordTextBox.TabIndex = 3;
             // 
             // loginButton
             // 
-            this.loginButton.BackColor = System.Drawing.Color.Transparent;
+            this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.FlatAppearance.BorderSize = 0;
             this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.loginButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.loginButton.Location = new System.Drawing.Point(100, 90);
+            this.loginButton.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(50, 320);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(200, 27);
+            this.loginButton.Size = new System.Drawing.Size(300, 38);
             this.loginButton.TabIndex = 4;
             this.loginButton.Text = "Войти";
             this.loginButton.UseVisualStyleBackColor = false;
@@ -89,19 +121,23 @@
             // 
             // LoginForm
             // 
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(358, 158);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.ClientSize = new System.Drawing.Size(400, 410);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.loginButton);
             this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.loginTextBox);
             this.Controls.Add(this.passwordLabel);
             this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.logobox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logobox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,5 +150,7 @@
         private System.Windows.Forms.TextBox loginTextBox;
         private System.Windows.Forms.TextBox passwordTextBox;
         private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.PictureBox logobox;
+        private System.Windows.Forms.Label label1;
     }
 }
--- WindowsFormsApp1/LoginForm.cs
+++ WindowsFormsApp1/LoginForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,22 @@ namespace WindowsFormsApp1
         public LoginForm()
         {
             InitializeComponent();
+
+            // Поднимаемся из bin\Debug\netX.X в папку WindowsFormsApp1 
+            // и заходим в вашу папку Resourses (через 's')
+            // Убираем одну пару точек, чтобы остаться внутри папки WindowsFormsApp1
+            string imagePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\Resourses\image.png"));
+
+            if (File.Exists(imagePath))
+            {
+                logobox.SizeMode = PictureBoxSizeMode.Zoom;
+                logobox.Image = Image.FromFile(imagePath);
+            }
+            else
+            {
+                MessageBox.Show($"Опять не совпал путь. Программа искала файл тут:\n{imagePath}\n\nПроверьте, совпадает ли регистр букв и расширение (.png)!");
+            }
+
         }
 
         private void LoginButton_Click(object sender, EventArgs e)
@@ -32,6 +49,11 @@ namespace WindowsFormsApp1
             {
                 MessageBox.Show("Неверный логин или пароль.");
             }
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+           
         }
     }
 }
